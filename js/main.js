a3ab1aaaebcc42f29c0d41dd3ca3fc48a668612e
jazzbank <hasibhossain2006@gmail.com>
2025-01-08 17:11:53 -0500

    Added a bunch of stuff.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("Hello world!")
+var player = {
+    currencies: {
+        amount: new ExpantaNum(0)
+    },
+
+    mainUpgrade: {
+        level: new ExpantaNum(0),
+        bought: new ExpantaNum(0),
+    }
+}
+
+function increment() {
+    player.currencies.amount = ExpantaNum.add(player.currencies.amount, 1)
+}
+
+function update() {
+    document.getElementById("points").innerHTML = player.currencies.amount.toFixed(2)
+}
+
+window.setInterval(update, 1000/30)
