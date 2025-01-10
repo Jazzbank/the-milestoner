f9824a5b6a9d86acdc4a0b761b276adcd1a1704d
jazzbank <hasibhossain2006@gmail.com>
2025-01-09 21:55:49 -0500

    I have to redownload Arch Linux. Crap.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,58 @@
 var player = {
     currencies: {
-        amount: new ExpantaNum(0)
+        amount: new ExpantaNum(10)
     },
 
     mainUpgrade: {
         level: new ExpantaNum(0),
         bought: new ExpantaNum(0),
-    }
+        cost: new ExpantaNum(10)
+    },
+
+    milestones: {},
+
 }
 
+const FRAMERATE = 20
+
+function load() {
+    var milestoneInfo = [
+        // [starting level, repetition, effect, description, visibility]
+        [1, 1, 0, "Increases point generation by 1 every rank.", true],
+    ]
+
+    for (let i = 0; i < milestoneInfo.length; i++) {
+        Object.assign(player.milestones, {[i+1]: {
+            start: milestoneInfo[i][0],
+            repeat: milestoneInfo[i][1],
+            effect: milestoneInfo[i][2],
+            description: milestoneInfo[i][3],
+            visible: milestoneInfo[i][4]
+        }})
+    }
+
+
+}
+
+load()
+
 function increment() {
-    player.currencies.amount = ExpantaNum.add(player.currencies.amount, 1)
+    player.mainUpgrade.level = ExpantaNum.add(player.mainUpgrade.level, 1)
+}
+
+function prodLoop() {
+    player.currencies.amount = ExpantaNum.add(player.currencies.amount, ExpantaNum.div(player.mainUpgrade.level, FRAMERATE))
 }
 
 function update() {
-    document.getElementById("points").innerHTML = player.currencies.amount.toFixed(2)
+    document.getElementById("mainUpgLevel").innerHTML = player.mainUpgrade.level + " → " + ExpantaNum.add(player.mainUpgrade.level, 1)
+    document.getElementById("mainUpgCost").innerHTML = player.mainUpgrade.cost.toFixed(2)
+    document.getElementById("points").innerHTML = player.currencies.amount.toFixed(2) + " points"
 }
 
-window.setInterval(update, 1000/30)
+function mainLoop() {
+    prodLoop()
+    update()
+}
+
+window.setInterval(mainLoop, 1000/FRAMERATE)
